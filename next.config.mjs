d17ffab822b1b6db872b7c460c45d2134b921dd5
Vatey <thavryvateyphal113@gmail.com>
2026-07-03 15:17:47 +0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['192.168.2.2', 'localhost'],
 };
 
 export default nextConfig;
